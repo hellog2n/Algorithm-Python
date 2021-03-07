51b67c9b366a7d119269a9aa93d2fc7c5d7e2f31
hellog2n <wkddbjyjmail@gmail.com>
2021-03-07 16:44:43 +0900

    d

--- venv/page92.py
+++ venv/page92.py
@@ -13,13 +13,15 @@ def solution():
         m -= 1
     return sum
 
+
+
 def solution2():
     n, m, k = map(int, input().split())
     array = list(map(int, input().split()))
     sum = 0
     array.sort()
-    maxNum = array[-1]  # 최대값
-    max2Num = array[-2]
+    maxNum = array[-1]  # 첫번째로 큰 값
+    max2Num = array[-2] # 두번째로 큰 값
     sum = (m // (k + 1)) * (maxNum * k + max2Num) + (m % ( k + 1 )) * maxNum
     return sum
 
